d743545fba6a50bc2e455bd5ce65c70b2802461d
mclaughlin-dani <mclaughlin.dani@northeastern.edu>
2026-03-07 21:11:05 -0500

    feat: add tic tac toe impl for automated opponent and game forfeit

---
+++ server/src/games/ticTacToe.ts
@@ -0,0 +1,223 @@
+import {
+  zTicTacToeMove,
+  type TicTacEntry,
+  type TicTacToeState,
+  type TicTacToeView,
+} from "@gamenite/shared";
+import type { GameLogic } from "./gameLogic.ts";
+import { GameService } from "./gameServiceManager.ts";
+
+// Utility type for a tic tac toe board, used to guarantee board dimentions
+// and entries for helper functions. Private to this file.
+type TicTacToeBoard = [
+  [TicTacEntry, TicTacEntry, TicTacEntry],
+  [TicTacEntry, TicTacEntry, TicTacEntry],
+  [TicTacEntry, TicTacEntry, TicTacEntry],
+];
+
+const NUM_PLAYERS: number = 2;
+
+// Player 0 (Player #1 in the front-end) starts the game, and goes second (O).
+// Player 1 (Player #2 in the front-end) joins the game after it is started, and goes first (X).
+const PLAYER_IDX_TO_ENTRY_MAP: [TicTacEntry, TicTacEntry] = ["O", "X"];
+
+// List of all possible winning indexes (rows, columns, diagonals) for ease of testing
+const WINNING_COORDS: [[number, number], [number, number], [number, number]][] = [
+  [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ],
+  [
+    [1, 0],
+    [1, 1],
+    [1, 2],
+  ],
+  [
+    [2, 0],
+    [2, 1],
+    [2, 2],
+  ],
+  [
+    [0, 0],
+    [1, 0],
+    [2, 0],
+  ],
+  [
+    [0, 1],
+    [1, 1],
+    [2, 1],
+  ],
+  [
+    [0, 2],
+    [1, 2],
+    [2, 2],
+  ],
+  [
+    [0, 0],
+    [1, 1],
+    [2, 2],
+  ],
+  [
+    [0, 2],
+    [1, 1],
+    [2, 0],
+  ],
+];
+
+/**
+ * Checks the given coordinates to determine if all three are equal to
+ * the specified TicTacEntry. Assumes that coordinates given are within range,
+ * as this is a private helper function.
+ *
+ * @param board the board to check for wins
+ * @param c1 the first coordinate on the board to check
+ * @param c2 the second coordinate on the board to check
+ * @param c3 the third coordinate on the board to check
+ * @param symbol the specific TicTacEntry being checked
+ * @returns true if the three coordinates all have the same entry
+ */
+function checkCoordsForEquality(
+  board: TicTacToeBoard,
+  c1: [number, number],
+  c2: [number, number],
+  c3: [number, number],
+  symbol: TicTacEntry,
+) {
+  return (
+    board[c1[0]][c1[1]] === symbol &&
+    board[c2[0]][c2[1]] === symbol &&
+    board[c3[0]][c3[1]] === symbol
+  );
+}
+
+/**
+ * Checks the whole board to determine if there is a win for the given entry.
+ *
+ * @param board the board to check
+ * @param entry the entry to compare to
+ * @returns true if a win exists for the given entry.
+ */
+function checkWinbyEntry(board: TicTacToeBoard, entry: TicTacEntry) {
+  for (let i = 0; i < WINNING_COORDS.length; i += 1) {
+    const [a, b, c] = WINNING_COORDS[i];
+    if (checkCoordsForEquality(board, a, b, c, entry)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * Determine if the board is full of entries, or if there are still playable places.
+ *
+ * @param board the board to check
+ * @returns true if full, false if not
+ */
+function isBoardFull(board: TicTacToeBoard) {
+  for (let i = 0; i < board.length; i += 1) {
+    if (
+      board[i].filter(function (v) {
+        return v === null;
+      }).length > 0
+    )
+      return false;
+  }
+  return true;
+}
+
+/**
+ * Finds a valid winning entry for either player if one exists. Assumes the board is in
+ * a valid state (there are not winning entries for both players).
+ *
+ * @param board the board to check
+ * @returns a winning entry if one exists, otherwise null
+ */
+function getWinningEntry(board: TicTacToeBoard) {
+  for (let i = 0; i < WINNING_COORDS.length; i += 1) {
+    const [a, b, c] = WINNING_COORDS[i];
+    if (
+      checkCoordsForEquality(board, a, b, c, "X") ||
+      checkCoordsForEquality(board, a, b, c, "O")
+    ) {
+      return WINNING_COORDS[i];
+    }
+  }
+  return null;
+}
+
+/**
+ * Helper function to determine if a game is done.
+ *
+ * @param board the board representing the current game state
+ * @returns true if the game is done, otherwise false
+ */
+function isGameDone(board: TicTacToeBoard) {
+  return checkWinbyEntry(board, "X") || checkWinbyEntry(board, "O") || isBoardFull(board);
+}
+
+export const ticTacToeLogic: GameLogic<TicTacToeState, TicTacToeView> = {
+  minPlayers: 2,
+  maxPlayers: 2,
+  start: () => ({
+    board: [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ],
+    nextPlayer: 1,
+  }),
+  update: (state, payload, playerIndex) => {
+    const move = zTicTacToeMove.safeParse(payload);
+    if (move.error) return null;
+
+    const [i, j] = move.data;
+
+    // reject moves that would overwrite existing moves
+    if (state.board[i][j] !== null) return null;
+
+    // reject moves from the wrong player
+    if (playerIndex !== state.nextPlayer) return null;
+
+    // reject moves if the game is already done
+    if (isGameDone(state.board)) return null;
+
+    const newBoard: TicTacToeBoard = [...state.board];
+
+    newBoard[i][j] = PLAYER_IDX_TO_ENTRY_MAP[playerIndex];
+
+    const nextState: TicTacToeState = {
+      board: newBoard,
+      nextPlayer: (state.nextPlayer + 1) % NUM_PLAYERS,
+    };
+
+    return nextState;
+  },
+  isDone: (state) => {
+    return isGameDone(state.board);
+  },
+  viewAs: (state) => {
+    const stateView: TicTacToeView = {
+      board: state.board,
+      nextPlayer: state.nextPlayer,
+      winningEntry: getWinningEntry(state.board),
+    };
+    return stateView;
+  },
+  tagView: (view) => ({ type: "tictactoe", view }),
+  describeMove: (_prevState, newState, payload) => {
+    const move = zTicTacToeMove.parse(payload);
+    const [i, j] = move;
+
+    // next move is a winning move, assumes this is not called after game is over with additional moves
+    if (isGameDone(newState.board) && getWinningEntry(newState.board)) {
+      return ` moved at (${i}, ${j}) and won the game`;
+    }
+    if (isBoardFull(newState.board)) {
+      return ` moved at (${i}, ${j}) and ended the game in a draw`;
+    }
+    return ` moved at (${i}, ${j})`;
+  },
+};
+
+export const ticTacToeGameService = new GameService<TicTacToeState, TicTacToeView>(ticTacToeLogic);
---
+++ shared/src/games/ticTacToe.ts
@@ -0,0 +1,103 @@
+import { z } from "zod";
+
+export type TicTacEntry = null | "O" | "X";
+
+/**
+ * A tic-tac-toe board state is a 3x3 array where each entry is either null, X, or O.
+ *
+ * We always let player 1 go first, and assign them the `X` entry. Player 0 always
+ * goes second and is the `O` entry. Therefore, the board always starts out looking like
+ * this:
+ *
+ *     {
+ *       board: [
+ *         [null, null, null],
+ *         [null, null, null],
+ *         [null, null, null],
+ *       ],
+ *       nextPlayer: 1,
+ *     }
+ */
+export interface TicTacToeState {
+  board: [
+    [TicTacEntry, TicTacEntry, TicTacEntry],
+    [TicTacEntry, TicTacEntry, TicTacEntry],
+    [TicTacEntry, TicTacEntry, TicTacEntry],
+  ];
+  nextPlayer: number;
+}
+
+/**
+ * Tic-tac-toe, like Nim, gives every player the same information, so every
+ * player (and people who are not playing) have the same view.
+ *
+ * If someone has already won, then **no more moves are allowed**, and the
+ * view should contain this information as `winningEntry`. If the board looks
+ * like this:
+ *
+ *    [ [ 'X', 'O', null ],
+ *      [ 'X', null, 'O' ],
+ *      [ 'X', null, null ] ]
+ *
+ * then winningEntry should be `[[0,0], [1,0], [2,0]]`. (Or some other permutation,
+ * like `[[2,0], [1,0], [0, 0]]`.)
+ *
+ * If no one has triumphed yet, for example if the board looks like this:
+ *
+ *    [ [ 'X', 'O', null ],
+ *      [ null, null, 'O' ],
+ *      [ 'X', null, null ] ]
+ *
+ * then winningEntry should be `null`.
+ *
+ * If there are multiple winning positions, any winning position can
+ * be returned.
+ */
+export interface TicTacToeView {
+  board: [
+    [TicTacEntry, TicTacEntry, TicTacEntry],
+    [TicTacEntry, TicTacEntry, TicTacEntry],
+    [TicTacEntry, TicTacEntry, TicTacEntry],
+  ];
+  nextPlayer: number;
+  winningEntry: null | [[number, number], [number, number], [number, number]];
+}
+
+/**
+ * A move in tic-tac-toe is a two-element list of numbers between 0 and 2
+ * (inclusive), representing an entry in the TicTacToe board where the current
+ * player wants to put a mark.
+ *
+ * If the board looks like this:
+ *
+ *     XX.
+ *     OO.
+ *     XOX
+ *
+ * and it is O's turn (player 0), then the winning move is [1,2]. If player 0
+ * makes the winning move [1,2], then the new board state looks like this:
+ *
+ *     XX.
+ *     OOO
+ *     XOX
+ *
+ * Moves can only be played in spaces without marks, so the only valid move O
+ * can make at this point is [0,2], which results in a board that looks like
+ * this:
+ *
+ *     XXO
+ *     OO.
+ *     XOX
+ *
+ * This is a stalemate! It will now be X's turn (that is, player 1's turn),
+ * and the only valid move they can make is [1,2], which results in this
+ * board, which has no winners:
+ *
+ *     XXO
+ *     OOX
+ *     XOX
+ *
+ */
+export type TicTacToeMove = z.infer<typeof zTicTacToeMove>;
+const tttPos = z.int().gte(0).lt(3);
+export const zTicTacToeMove = z.tuple([tttPos, tttPos]);
