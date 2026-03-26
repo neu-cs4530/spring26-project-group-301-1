ddb1c5eeefb42f2966927deed6d3fc9016fc3b81
mclaughlin-dani <mclaughlin.dani@northeastern.edu>
2026-03-25 23:59:23 -0400

    Add back docstrings for TTT methods

--- shared/src/games/ticTacToeUtils.ts
+++ shared/src/games/ticTacToeUtils.ts
@@ -53,6 +53,18 @@ export const WINNING_COORDS: [[number, number], [number, number], [number, numbe
   ],
 ];
 
+/**
+ * Checks the given coordinates to determine if all three are equal to
+ * the specified TicTacEntry. Assumes that coordinates given are within range,
+ * as this is a private helper function.
+ *
+ * @param board the board to check for wins
+ * @param c1 the first coordinate on the board to check
+ * @param c2 the second coordinate on the board to check
+ * @param c3 the third coordinate on the board to check
+ * @param symbol the specific TicTacEntry being checked
+ * @returns true if the three coordinates all have the same entry
+ */
 export function checkCoordsForEquality(
   board: TicTacToeBoard,
   c1: [number, number],
@@ -67,6 +79,13 @@ export function checkCoordsForEquality(
   );
 }
 
+/**
+ * Checks the whole board to determine if there is a win for the given entry.
+ *
+ * @param board the board to check
+ * @param entry the entry to compare to
+ * @returns true if a win exists for the given entry.
+ */
 export function checkWinByEntry(board: TicTacToeBoard, entry: TicTacEntry) {
   for (let i = 0; i < WINNING_COORDS.length; i += 1) {
     const [a, b, c] = WINNING_COORDS[i];
@@ -75,6 +94,12 @@ export function checkWinByEntry(board: TicTacToeBoard, entry: TicTacEntry) {
   return false;
 }
 
+/**
+ * Determine if the board is full of entries, or if there are still playable places.
+ *
+ * @param board the board to check
+ * @returns true if full, false if not
+ */
 export function isBoardFull(board: TicTacToeBoard) {
   for (let i = 0; i < board.length; i += 1) {
     if (board[i].some((v) => v === null)) return false;
@@ -82,6 +107,13 @@ export function isBoardFull(board: TicTacToeBoard) {
   return true;
 }
 
+/**
+ * Finds a valid winning entry for either player if one exists. Assumes the board is in
+ * a valid state (there are not winning entries for both players).
+ *
+ * @param board the board to check
+ * @returns a winning entry if one exists, otherwise null
+ */
 export function getWinningEntry(board: TicTacToeBoard) {
   for (let i = 0; i < WINNING_COORDS.length; i += 1) {
     const [a, b, c] = WINNING_COORDS[i];
@@ -95,6 +127,12 @@ export function getWinningEntry(board: TicTacToeBoard) {
   return null;
 }
 
+/**
+ * Helper function to determine if a game is done.
+ *
+ * @param board the board representing the current game state
+ * @returns true if the game is done, otherwise false
+ */
 export function isGameDone(state: TicTacToeState) {
   return (
     state.forfeited === true ||
